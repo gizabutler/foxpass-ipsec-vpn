899097ea702b85f9b9ce84d45875d6803c200b7f
Sean Walsh <sean@kasisto.com>
2019-03-21 10:14:19 -0400

    updated network config script and EIP heartbeat resource file

--- scripts/network-config.py
+++ scripts/network-config.py
@@ -7,7 +7,6 @@ from os import chown, chmod, geteuid, urandom
 
 
 METADATA_BASE_URL = "http://169.254.169.254/"
-#private_ip = urlopen(METADATA_BASE_URL + 'latest/meta-data/local-ipv4').read()
 private_ip = ['172.16.1.100', '172.16.0.100']
 
 
@@ -19,29 +18,42 @@ def get_address(private_ip):
                      adapter + '/local-ipv4s/').read()
         if ip in private_ip:
             return ip
+# TODO: while loop to wait for ENI
 
 
 address = get_address(private_ip)
 
 
+def get_gateway(address):
+    if address == '172.16.0.100':
+        gateway = '172.16.0.1'
+        return gateway
+    elif address == '172.16.1.100':
+        gateway = '172.16.1.1'
+        return gateway
+
+
+gateway = get_gateway(address)
+
+
 def write_to_cfg(address):
-    # write IP `to ens6.cfg
-    file_loader = FileSystemLoader("/home/ubuntu/")
+    # * write IP `to eth1.cfg
+    file_loader = FileSystemLoader("/opt/templates/")
     env = Environment(loader=file_loader)
-    template = env.get_template('ens6.cfg')
-    output = template.render(PRIVATE_IP=address)
-    fname = "/etc/network/interfaces.d/ens6.cfg"
+    template = env.get_template('eth1.cfg')
+    output = template.render(PRIVATE_IP=address, GATEWAY=gateway)
+    fname = "/etc/network/interfaces.d/eth1.cfg"
     with open(fname, 'w') as f:
         f.write(output)
     return output
-    # set /etc/network/interfaces.d/ens6.cfg to be owned and only accessible by root
-    chmod('/etc/network/interfaces.d/ens6.cfg', 0600)
-    chown('/etc/network/interfaces.d/ens6.cfg', 0, 0)
-
+    # * set /etc/network/interfaces.d/eth1.cfg to be owned and only accessible by root
+    chmod('/etc/network/interfaces.d/eth1.cfg', 0600)
+    chown('/etc/network/interfaces.d/eth1.cfg', 0, 0)
+# TODO: write gateway to templated file
 
 output = write_to_cfg(address)
 
-
+# ! not working
 def restart_services():
     subprocess.call(['systemctl', 'restart', 'networking'], shell=False)
     subprocess.call(['systemctl', 'start', 'pacemaker'], shell=False)
@@ -50,6 +62,7 @@ def restart_services():
 
 def main():
     get_address
+    get_gateway
     write_to_cfg
     restart_services
 
